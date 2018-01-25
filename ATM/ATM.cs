27b2724deb7df9988e77cbe91dba443b5ce0f81d
mjlacy <mjlacy.dude@gmail.com>
2018-01-25 10:36:22 -0600

    Add project files.

---
+++ ATM/ATM.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATM
+{
+    class ATM
+    {
+        static int numberOfAccounts = 3;
+        private Account[] myAccounts = new Account[numberOfAccounts];
+        static void Main(string[] args)
+        {
+            ATM atm = new ATM();
+            //atm.start();
+            atm.greeting();
+            atm.runATM();
+        }
+
+        void greeting()
+        {
+            Console.WriteLine("Welcome, is this your first time using the ATM? (Y/N)");
+            String input = Console.ReadLine();
+            if (input == "y" || input == "Y")
+            {
+                populateAcct();
+            }
+            else if (input == "n" || input == "N")
+            {
+                //the main method or runATM will take it to runATM
+            }
+            else
+            {
+                Console.WriteLine("\nPlese type in Y or N, either case is acceptable\n");
+                greeting();
+            }
+        }
+
+        void runATM()
+        {
+            try
+            {
+                selectAcct();
+            }
+            catch (Exception exc)
+            {
+                Console.WriteLine("\nInvalid Input\n");
+            }
+            greeting();
+            runATM();
+        }
+
+        void populateAcct()
+        {
+            int index = -99;
+            for (int i = 0; i < myAccounts.Length; i++)
+            {
+                if (myAccounts[i] == null)
+                {
+                    index = i;
+                    break;
+                }
+            }
+            if (index != -99)
+            {
+                Console.WriteLine("\nPlease enter a name to be associated with the type of account you want");
+                String name = Console.ReadLine();
+                for (int i = 0; i < myAccounts.Length; i++)
+                {
+                    if (myAccounts[i] != null && name == myAccounts[i].getName())
+                    {
+                        Console.WriteLine("\nThat name has already been used\n");
+                        greeting();
+                        return;
+                    }
+
+                    if (name == "")
+                    {
+                        Console.WriteLine("You must enter a name\n");
+                        greeting();
+                        return;
+                    }
+                }
+                Console.WriteLine("\nPlease enter a PIN to use with this account");
+                String PIN = Console.ReadLine();
+
+                if (PIN == "")
+                {
+                    Console.WriteLine("You must enter a PIN\n");
+                    greeting();
+                    return;
+                }
+                /*
+                Console.WriteLine("\nWhat type of account would you like to open, checking, savings, or super saver?");
+                String type = Console.ReadLine();
+                
+                if (type.equalsIgnoreCase("checking"))
+                {
+                    myAccounts[index] = new Checking(100, index, name, PIN);
+                    Console.WriteLine("\nBalances start at $100, and the annual interest rate is 5%");
+                }
+                else if (type.equalsIgnoreCase("savings"))
+                {
+                    myAccounts[index] = new Savings(100, index, name, PIN);
+                    Console.WriteLine("\nBalances start at $100, and the annual interest rate is 30%");
+                }
+                else
+                {
+                    Console.WriteLine("\nInvalid account type");
+                    populateAcct();
+                }*/
+                myAccounts[index] = new Account(100, index, name, PIN);
+                Console.WriteLine("\nBalances start at $100, and the annual interest rate is 5%");
+            }
+            else
+            {
+                Console.WriteLine("\nSorry, all available accounts are full\n");
+                greeting();
+            }
+        }
+        void selectAcct()
+        {
+            int num = -99;
+            String name, PIN;
+
+            try
+            {
+                Console.WriteLine("\nEnter your account name");
+                name = Console.ReadLine();
+                Console.WriteLine("\nEnter your PIN");
+                PIN = Console.ReadLine();
+                for (int i = 0; i < myAccounts.Length; i++)
+                {
+                    if (myAccounts[i] != null && name == myAccounts[i].getName() && PIN == myAccounts[i].getPIN())
+                    {
+                        num = i;
+                        break;
+                    }
+                }
+                Console.WriteLine();
+                myAccounts[num].Menu();
+            }
+            catch (Exception exc)
+            {
+                Console.WriteLine("\nInvalid Input");
+            }
+        }
+    }
+}
---
+++ ATM/Account.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATM
+{
+    class Account
+    {
+        decimal balance;
+        int acctNumber;
+        string name;
+        string PIN;
+
+        public Account(int bal, int num, String name, String PIN)
+        {
+            balance = bal;
+            acctNumber = num;
+            this.name = name;
+            this.PIN = PIN;
+        }
+
+        public String getName()
+        {
+            return name;
+        }
+
+        public String getPIN()
+        {
+            return PIN;
+        }
+
+        public void Menu()
+        {
+            string input;
+            do
+            {
+                Console.WriteLine("|==================================|");
+                Console.WriteLine("|        FIRST NATIONAL BANK       |");
+                Console.WriteLine("|***********Menu Options***********|");
+                Console.WriteLine("|__________________________________|");
+                Console.WriteLine("|  Press 1 To Make Deposit         |");
+                Console.WriteLine("|  Press 2 To Make Withdrawl       |");
+                Console.WriteLine("|  Press 3 To View Current Balance |");
+                Console.WriteLine("|  Press 4 To Save/Exit            |");
+                Console.WriteLine("|__________________________________|");
+                Console.WriteLine("|   Please Make Selection Now...   |");
+                Console.WriteLine("|==================================|");
+                Console.WriteLine();
+
+                input = Console.ReadLine();
+
+                Console.WriteLine();
+
+                if (input == "1")
+                {
+                    Deposit();
+                }
+                else if (input == "2")
+                {
+                    Withdraw();
+                }
+                else if (input == "3")
+                {
+                    CheckBalance();
+                }
+                else if (input == "4")
+                {
+                    //do nothing
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input, please choose 1, 2, 3, or 4");
+                }
+                Console.WriteLine();
+            } while (input != "4");
+        }
+
+        void Deposit()
+        {
+            Console.WriteLine("\nHow much would you like to deposit? $");
+            decimal deposit = 0.00m;
+            try
+            {
+                deposit = decimal.Parse(Console.ReadLine());
+            }
+            catch(FormatException ex)
+            {
+                Console.WriteLine("Deposits must be a number");
+                Deposit();
+                return;
+            }
+            if (deposit > 0.00m)
+            {
+                balance += deposit;
+                Console.WriteLine("\nDeposit of " + string.Format("{0:C}", deposit) + " successful");
+            }
+            else
+            {
+                Console.WriteLine("\nYou must enter a number greater than 0.00");
+            }
+        }
+
+        void Withdraw()
+        {
+            Console.WriteLine("\nHow much would you like to withdraw? $");
+            decimal withdrawl = 0.00m;
+            try
+            {
+                withdrawl = decimal.Parse(Console.ReadLine());
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Withdrawls must be a number");
+                Withdraw();
+                return;
+            }
+            if (withdrawl > 0.00m)
+            {
+                if (withdrawl <= balance)
+                {
+                    balance -= withdrawl;
+                    Console.WriteLine("\nHere is " + string.Format("{0:C}", withdrawl));
+                }
+                else
+                {
+                    Console.WriteLine("\nInsufficent Funds");
+                }
+            }
+            else
+            {
+                Console.WriteLine("\nYou must enter a number greater than 0.00");
+            }
+        }
+
+        void CheckBalance()
+        {
+            Console.WriteLine("\nYour balance is: " + string.Format("{0:C}", balance));
+        }
+    }
+}
